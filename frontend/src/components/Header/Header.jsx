095a5c8b0d84a14729b8d12e7fe50539cf70abf0
Harshitha <harshithapalaram09@gmail.com>
2026-02-06 13:47:06 +0530

    Fix header image using JSX import

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Header.css'
+import headerImg from '../assets/header_img.png';
+
 const Header = () => {
   return (
     <div>
-      <div className="header">
+      <div className="header"
+      style={{
+          backgroundImage: `url(${headerImg})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'contain',
+          height: '34vw',
+          margin: '30px auto',
+          position: 'relative',
+        }}
+      >
+
         <div className="header-contents">
             <h2>Order Your Favourite Food Here</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience!</p>
